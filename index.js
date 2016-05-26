6cf9eabdd60ee76a78cab96cf2be5d53abdfabe8
pdeng <pdeng@thinkjoy.cn>
2016-05-27 01:27:38 +0800

    rewirt file

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+// var express = require('express');
+//var stormpath = require('express-stormpath');
+// var app = express();
+// app.use(stormpath.init(app,function(){
+//   application:process.env.STORMPATH_URL
+// }))
+//
+// app.get('/',function(req,res){
+//   res.send('hell heroku...')
+// })
+
 var express = require('express');
-var stormpath = require('express-stormpath');
 var app = express();
-app.use(stormpath.init(app,function(){
-  application:process.env.STORMPATH_URL
-}))
-app.get('/',function(req,res){
-  res.send('hell heroku')
-})
+
+app.get('/', function (req, res) {
+  res.send('hell heroku...');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
